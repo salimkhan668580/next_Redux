2914b33105d4751b1e4a9b14aeb2e985c63e3b4c
salim khan <salimkhan668580s@gmail.com>
2025-10-01 14:43:28 +0530

    add avatar

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -7,9 +7,13 @@ function Todo() {
 
   return (
     <div className="max-w-md mx-auto mt-10 p-6 bg-gray-100 rounded-lg shadow-md">
+      <div className="flex item-center justify-center gap-2">
+
       <h1 className="text-2xl font-bold mb-6 text-center text-gray-800">
         Todo App
       </h1>
+        <img  className="w-10 h-10 rounded-full mr-4" src={`https://ui-avatars.com/api/?name=Salim%20khan&background=random&color=fff&size=40`} alt="" />
+      </div>
 
 
 
